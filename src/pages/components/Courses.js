c800334b4c2ab85b5f33d826dce335bb31ec3c2c
kovalishynmv <kovalishynmv@gmail.com>
2019-02-28 16:10:24 +0200

    v2.0.0
    Добавил квери

--- src/pages/components/Courses.js
+++ src/pages/components/Courses.js
@@ -1,38 +1,24 @@
-import React from 'react';
 import '../../index.scss';
 import Slider from "react-slick";
+import React, { Component } from "react";
 
-class Courses extends React.Component {
-    render() {
-      var settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      };
-      return (
+export default class MultipleItems extends Component {
+  render() {
+    const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    };
+    return (
+      <div>
+        <h2>Приклади освітніх подій</h2>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          <div className="courses__card"></div>
+          <div className="courses__card"></div>
+          <div className="courses__card"></div>
         </Slider>
-      );
-    }
+      </div>
+    );
   }
-
-  export default Courses;
+}
